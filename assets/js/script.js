aeef5c9fe22d0da94aa1c5d515fff8bbd28dd844
RyandTeague <ryanteague@hotmail.co.uk>
2022-05-17 10:12:03 +0000

    add favicon and tidy ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer()
+        }
+    })
+
     runGame("addition");
 });
 
@@ -23,6 +29,10 @@ document.addEventListener("DOMContentLoaded", function() {
  * user's answer has been processed
 */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     // Generates Two Random Numbers
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -111,8 +121,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "-";
 }
 
@@ -123,7 +133,18 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operand1").textContent = operand1
+    document.getElementById("operand2").textContent = (operand1 / operand2).isInteger ? operand2 : getDivideNumber();
     document.getElementById("operator").textContent = "/";
+}
+
+function getDivideNumber() {
+    let operand1 = parseInt(document.getElementById("operand1").innerText)
+    let operand2 = parseInt(document.getElementById("operand2").innerText)
+
+    do {
+        operand2 = Math.floor(Math.random() * 25) + 1;
+    } while (((operand1 / operand2).isInteger) = false)
+
+    return operand2
 }
